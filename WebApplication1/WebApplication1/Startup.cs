87b0a0c34e956a2fd2f373581ff5f8d52fd82ed6
kripagupta <37304248+kripagupta@users.noreply.github.com>
2020-10-30 16:18:22 +0530

    Update Startup.cs
    Testing purpose

--- WebApplication1/WebApplication1/Startup.cs
+++ WebApplication1/WebApplication1/Startup.cs
@@ -14,6 +14,7 @@ namespace WebApplication1
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        // kripa comment on Github to reflect for local machine
         public void ConfigureServices(IServiceCollection services)
         {
         }
